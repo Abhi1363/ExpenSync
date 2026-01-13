9f4d72bf08490d109483b1a5e22895e9669d6c43
Abhyuday Pawar <1362003abhi@gmail.com>
2026-01-13 16:55:17 +0530

    deployment bug1

--- frontend/src/utils/axiosInstance.jsx
+++ frontend/src/utils/axiosInstance.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.BACKEND_URL || "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/api",
 });
 
 axiosInstance.interceptors.request.use((config) => {
