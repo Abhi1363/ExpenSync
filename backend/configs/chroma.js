04a1c5ae835306ba1a7973f4887c14a454b1e1de
Abhyuday Pawar <1362003abhi@gmail.com>
2026-08-17 16:36:37 +0530

    chromadb deployment

--- backend/configs/chroma.js
+++ backend/configs/chroma.js
@@ -1,5 +1,5 @@
 import {ChromaClient} from "chromadb";
 
-export const chromaClient  = new ChromaClient({  
-    path:"http://localhost:8000"
-  })
+const chromaClient = new ChromaClient({
+    path: process.env.CHROMA_URL
+});
