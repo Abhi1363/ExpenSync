7b6c282d6a6af68c49d964d70d3d5111d4ea7625
Abhyuday Pawar <1362003abhi@gmail.com>
2026-08-18 11:49:17 +0530

    chroma db testing

--- backend/configs/chroma.js
+++ backend/configs/chroma.js
@@ -1,5 +1,7 @@
 import {ChromaClient} from "chromadb";
 
+console.log("CHROMA_URL:", process.env.CHROMA_URL);
+
 export const chromaClient = new ChromaClient({
     path: process.env.CHROMA_URL
 });
